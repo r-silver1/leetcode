d12bd6630aaad6f21878b897fafe301ea41a33c8
r-silver1 <r.elliot.silver@gmail.com>
2021-01-31 15:22:49 -0800

    got win working with lines, but not code cleaned up

--- src/com/example/leetcode/tictac/board/board.java
+++ src/com/example/leetcode/tictac/board/board.java
@@ -18,6 +18,7 @@ public class board extends Canvas {
     public static Canvas canvas = new board();
     public static Boolean xTurn = true;
     public static int[][] boardTiles = new int[3][3];
+    public static Boolean globalWin = false;
 
     public static void initBoard(){
         //application ends when JFrame closes
@@ -51,9 +52,14 @@ public class board extends Canvas {
         boardTiles[s.getRow()-1][s.getCol()-1] = tempInt;
     }
 
-    public static boolean checkWin(){
-        int[] colSum = new int[boardTiles.length];
+    //return int array: pos 0: 0 for no win; 1 for win
+    //                  pos 1: -1 for X, 1 for O
+    //                  pos 1-5: for drawing win line: C1R1, C2R2
 
+    public static int[] checkWin(){
+        int[] retArray;
+
+        int[] colSum = new int[boardTiles.length];
         //row wins
         for(int i = 0; i<boardTiles.length; i+=1){
             int rowSum = 0;
@@ -63,18 +69,28 @@ public class board extends Canvas {
             }
             //win O
             if(rowSum == boardTiles.length){
-                System.out.println("row win: " + (i+1) + " letter: O");
+//                System.out.println("row win: " + (i+1) + " letter: O");
+//                retArray = new int[]{1, 1, i, 1, i, boardTiles.length};
+                retArray = new int[]{1, 1, 1, i+1, boardTiles.length, i+1};
+                return retArray;
             }else if(rowSum == -boardTiles.length){
-                System.out.println("row in: " + (i+1) + "letter: X");
+//                System.out.println("row in: " + (i+1) + "letter: X");
+                retArray = new int[]{1, 1, 1, i+1, boardTiles.length, i+1};
+                return retArray;
             }
         }
 
         //column wins
         for(int i = 0; i < colSum.length; i+=1){
             if(colSum[i] == boardTiles.length){
-                System.out.println("col win: " + (i+1) + " letter: O");
+//                System.out.println("col win: " + (i+1) + " letter: O");
+//                retArray = new int[]{1, 1, 1, i+1, boardTiles.length, i+1};
+                retArray = new int[]{1, 1, i+1, 1, i+1, boardTiles.length};
+                return retArray;
             }else if(colSum[i] == -boardTiles.length){
-                System.out.println("col win: " + (i+1) + " letter: X");
+//                System.out.println("col win: " + (i+1) + " letter: X");
+                retArray = new int[]{1, 1, i+1, 1, i+1, boardTiles.length};
+                return retArray;
             }
         }
 
@@ -86,18 +102,29 @@ public class board extends Canvas {
             diagTopRight += boardTiles[i][boardTiles.length-1-i];
         }
         if(diagTopLeft == boardTiles.length){
-            System.out.println("diag win: TL, letter: O");
+//            System.out.println("diag win: TL, letter: O");
+//            retArray = new int[]{1, 1, 1, 1, boardTiles.length, boardTiles.length};
+            retArray = new int[]{1, 1, 1, 1, boardTiles.length, boardTiles.length};
+            return retArray;
         }else if(diagTopLeft == -boardTiles.length){
-            System.out.println("diag win: TL, letter: X");
+//            System.out.println("diag win: TL, letter: X");
+            retArray = new int[]{1, -1, 1, 1, boardTiles.length, boardTiles.length};
+            return retArray;
         }
 
         if(diagTopRight == boardTiles.length){
-            System.out.println("diag win: TR, letter: O");
+//            System.out.println("diag win: TR, letter: O");
+//            retArray = new int[]{1, 1, 1, boardTiles.length, boardTiles.length, 1};
+            retArray = new int[]{1, 1, boardTiles.length, 1, 1, boardTiles.length};
+            return retArray;
         }else if(diagTopRight == -boardTiles.length){
-            System.out.println("diag win: TR, letter: X");
+//            System.out.println("diag win: TR, letter: X");
+            retArray = new int[]{1, 1, boardTiles.length, 1, 1, boardTiles.length};
+            return retArray;
         }
 
-        return false;
+        retArray = new int[]{0,0,0,0,0,0};
+        return retArray;
     }
 
     //https://stackoverflow.com/questions/1692677/how-to-create-a-jbutton-with-a-menu/1693326#1693326
@@ -107,20 +134,22 @@ public class board extends Canvas {
             public void mouseClicked(MouseEvent e) {
                 int[] colRow = Decoder.cellDecode(e.getX(), e.getY(), canvas.getWidth(), canvas.getHeight());
 //                System.out.println("col, row: " + colRow[0] + "," + colRow[1]);
-                if(checkTile(colRow[0], colRow[1])) {
-                    String thisLetter;
-                    if (xTurn) {
-                        thisLetter = "X";
-                    } else {
-                        thisLetter = "O";
+                if(!globalWin) {
+                    if (checkTile(colRow[0], colRow[1])) {
+                        String thisLetter;
+                        if (xTurn) {
+                            thisLetter = "X";
+                        } else {
+                            thisLetter = "O";
+                        }
+                        xTurn = !xTurn;
+                        Shape thisTurn = new Shape(colRow[0], colRow[1], thisLetter);
+                        shapeList.add(thisTurn);
+                        updateTiles(thisTurn);
+                        Decoder.printBoard(boardTiles);
                     }
-                    xTurn = !xTurn;
-                    Shape thisTurn = new Shape(colRow[0], colRow[1], thisLetter);
-                    shapeList.add(thisTurn);
-                    updateTiles(thisTurn);
-                    Decoder.printBoard(boardTiles);
-                    canvas.repaint();
                 }
+                canvas.repaint();
             }
         });
     }
@@ -145,6 +174,19 @@ public class board extends Canvas {
 //            curr.print();
             g2.drawString(curr.getLetter(), Math.round((curr.getCol()*(this.getWidth()/3.0)-this.getWidth()/5.0)), Math.round((curr.getRow()*(this.getHeight()/2.7)-this.getHeight()/5.0)));
         }
-        checkWin();
+        int[] winCheck = checkWin();
+        if(winCheck[0] == 1){
+            globalWin = true;
+            System.out.print("win! : [");
+            for(int i : winCheck){
+                System.out.print(i +" ,");
+            }
+            System.out.println("]");
+            g2.setStroke(new BasicStroke(10));
+            g2.setColor(new Color(255, 0, 0));
+            g2.draw(new Line2D.Double(-this.getWidth()/6 + this.getWidth()*winCheck[2]/3.0, -this.getHeight()/6 + this.getHeight()*winCheck[3]/3.0, -this.getWidth()/6 + this.getWidth()*winCheck[4]/3.0, -this.getHeight()/6 + this.getHeight()*winCheck[5]/3.0));
+//            g2.draw(new Line2D.Double(50.0, 50.0, 130.0, 130.3));
+
+        }
     }
 }
